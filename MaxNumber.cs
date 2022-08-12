feed9ab66086baee0b2e5a0350b0491a743861c7
Vidhya100 <vidhyadarade1@gmail.com>
2022-08-12 13:26:08 +0530

    computing max number

---
+++ MaxNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMaxNumber
+{
+    public class MaxNumber
+    {
+        public int first, second, third;
+
+        public MaxNumber(int first, int second, int third)
+        {
+            this.first = first;
+            this.second = second;
+            this.third = third;
+        }
+        public int GetMaxNum()
+        {
+            if (first.CompareTo(second) == 1 && first.CompareTo(third) ==1)
+                return first;
+            else if (second.CompareTo(first) == 1 && second.CompareTo(third) == 1)
+                return second;
+            else
+                return third;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using GenericMaxNumber;
+
+namespace GenericmaxNumber
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Enter the First number: ");
+            int first= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            int second= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the third number: ");
+            int third = Convert.ToInt32(Console.ReadLine());
+
+            MaxNumber maxnumObj = new MaxNumber(first,second,third);
+            int maxNumber = maxnumObj.GetMaxNum();
+
+            Console.WriteLine("your max number {0}", maxNumber);
+        }
+    }
+}
